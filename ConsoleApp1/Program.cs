0c9db0d0e267a9f8ae36223847316f26a603ce69
newal <newal@DESKTOP-KA1DTEQ>
2023-08-23 13:26:52 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// task 1
+//Console.WriteLine("It's easy to win forgiveness for being wrong; \r\n\tbeing right is what gets you into real trouble.\r\n\t\t\t\t\tBjarne Stroustrup");
+
+// task 2
+//int numb = 90;
+//int perc  = 10;
+//int result = (numb * perc) / 100 ;
+//Console.WriteLine(result);
+
+// task 3
+//int num1 = 1;
+//int num2 = 5;
+//int num3 = 7;
+//int num4 = 8;
+
+//int number = (num1 * 1000) + (num2 * 100) + (num3  * 10) + (num4);
+
+//Console.WriteLine(number);
